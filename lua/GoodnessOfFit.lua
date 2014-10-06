15b3b0ad1ccec740a68bf8bf0d77265a5eac3e69
Antonio Gomes de Oliveira Junior <junior092006@hotmail.com>
2014-10-06 09:56:25 -0300

    Tried to implement pixelByPixel String #4
    Tried to implement pixelByPixel String #4

--- lua/GoodnessOfFit.lua
+++ lua/GoodnessOfFit.lua
@@ -52,6 +52,49 @@ pixelByPixel = function(cs1, cs2, attribute1, attribute2)
 	-- TODO: retorna a media
 end
 
+pixelByPixelString = function(cs1, cs2, attribute1, attribute2)
+	-- cs1 tem attribute1 cs1.cells[1][attribute1] ~= nil
+	-- attributes string
+	-- cs2 tem attribute2
+
+
+	if type(cs1) ~= "CellularSpace" then
+		incompatibleTypeError("#1", "CellularSpace", cs1)
+	end
+	
+	if type(cs2) ~= "CellularSpace" then
+		incompatibleTypeError("#2", "CellularSpace", cs2)
+	end
+	
+	if type(attribute1) ~= "string" then
+		incompatibleTypeError("#3", "String", attribute1)
+	end
+	
+	if type(attribute2) ~= "string" then
+		incompatibleTypeError("#4", "String", attribute2)
+	end
+	
+	if cs1.cells[1][attribute1] == nil then
+		customError("#3 is not a valid cell attribute of #1.")
+	
+	end
+	if cs1.cells[1][attribute1] == nil then
+		customError("#4 is not a valid cell attribute of #2.")
+	
+	end
+	
+	local counter = 0
+	local equal = 0
+
+	forEachCellPair(cs1, cs2, function(cell1, cell2) 
+    		if cell1[attribute1] == cell2[attribute2] then
+			equal = equal + 1		
+		end
+		counter = counter + 1
+	end)
+	
+	return equal/counter
+end
 
 multiLevel = function(cs1, cs2, attribute)
 	
