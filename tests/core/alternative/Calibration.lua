661ae8d8bb7689dc6ea41bbe97941ff3f0aee2fb
agojunior <junior092006@hotmail.com>
2014-11-28 17:05:42 -0200

    Invalid Parameters verification implemented and finalTime is mandatory #14 close #19

--- tests/core/alternative/Calibration.lua
+++ tests/core/alternative/Calibration.lua
@@ -1,5 +1,59 @@
 
 return{
-	Calibration = function()
+	Calibration = function(unitTest)
+		local MyModel = Model{
+			x = 1,
+			setup = function(self)
+				self.t = Timer{
+					Event{action = function()
+						self.value = 2 * self.x ^2 - 3 * self.x + 4
+					end}
+				}
+			end
+		}
+
+		local error_func = function()
+			c = Calibration{
+				finalTime = 1,
+				parameters = {x ={ min = -100, max = 100}},
+				fit = function(model)
+					return model.value
+				end
+			}
+		end
+
+		unitTest:assert_error(error_func, mandatoryArgumentMsg("model"))	
+		local error_func = function()
+			c = Calibration{
+				model = MyModel,
+				parameters = {x ={ min = -100, max = 100}},
+				fit = function(model)
+					return model.value
+				end
+			}
+		end
+
+		unitTest:assert_error(error_func, mandatoryArgumentMsg("finalTime"))	
+		local error_func = function()
+			c = Calibration{
+				model = MyModel,
+				finalTime = 1,
+				fit = function(model)
+					return model.value
+				end
+			}
+		end
+		
+		unitTest:assert_error(error_func, mandatoryArgumentMsg("parameters"))	
+		local error_func = function()
+			local c = Calibration{
+				model = MyModel,
+				finalTime = 1,
+				parameters = {x ={ min = -100, max = 100}},
+			}
+			c:fit(model, parameters)
+		end
+		
+		unitTest:assert_error(error_func, "Function 'fit' was not implemented.")	
 	end
 }
--- tests/core/basic/Calibration.lua
+++ tests/core/basic/Calibration.lua
@@ -1,5 +1,32 @@
-
+require("calibration")
 return{
-	Calibration = function()
+Calibration = function(unitTest)
+
+local MyModel = Model{
+	x = 1,
+	setup = function(self)
+		self.t = Timer{
+			Event{action = function()
+				self.value = 2 * self.x ^2 - 3 * self.x + 4
+			end}
+		}
 	end
 }
+
+
+local c = Calibration{
+	model = MyModel,
+	finalTime = 1,
+	parameters = {x ={ min = -100, max = 100}},
+	fit = function(model)
+		return model.value
+	end
+}
+
+local result = c:execute()
+
+unitTest:assert_equal(result, 3)
+
+end
+}
+
