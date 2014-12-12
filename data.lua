80a3c97e267b3c3dff2b0adf09ae36ce8479902d
agojunior <junior092006@hotmail.com>
2014-12-12 14:08:43 -0200

    Calibration type now works if given multiple values to be tested instead of a range.

--- data.lua
+++ data.lua
@@ -1,9 +1,9 @@
-Data{
-	file = ["Costanza.map","Costanza2.map"]
-	summary = "Examples Cellular Spaces for discreteCostanzaMultiLevel Tests"
-	source = "Costanza's Paper  Model Goodness of Fit: a multiple resolution procedure"
-	attributes = ["Costanza","Costanza2"]
-	types = ["number", "number"]
-	description = ["Attribute values that will be compared", "Attribute values that will be compared"]
+data{
+	file = {"Costanza.map","Costanza2.map"},
+	summary = "Examples Cellular Spaces for discreteCostanzaMultiLevel Tests.",
+	source = "Costanza's Paper  Model Goodness of Fit: a multiple resolution procedure",
+	attributes = {"Costanza","Costanza2"},
+	types = {"number", "number"},
+	description = {"Attribute values that will be compared", "Attribute values that will be compared"},
 	reference = "Costanza's Paper  Model Goodness of Fit: a multiple resolution procedure"
 }
--- lua/Calibration.lua
+++ lua/Calibration.lua
@@ -1,10 +1,10 @@
-local executeRecursiveAux 
+local rangeRecursive 
 -- function used in execute to test the model with all the possible combinations of parameters.
 -- Params: Table with all the parameters and it's ranges indexed by number. In the example: Params[1] = {x, -100, 100}
 -- best: The smallest fitness of the model tested.
 -- a: the parameter that the function is currently variating. In the Example: 1 => x, 2=> y.
 -- Variables: The value that a parameter is being tested. Example: Variables = {x = -100, y = 1}
-executeRecursiveAux = function(self, Params, best, a, variables)
+rangeRecursive = function(self, Params, best, a, variables)
 		for parameter = Params[a]["min"],  Params[a]["max"] do	-- Testing the parameter in it's range.
 			variables[Params[a]["id"]] = parameter -- giving the variables table the current parameter and value being tested.
 			local mVariables = {} -- copy of the variables table to be used in the model.
@@ -21,13 +21,36 @@ executeRecursiveAux = function(self, Params, best, a, variables)
 				end
 
 			else  -- else, go to the next parameter to test it with it's range of values.
-					best = executeRecursiveAux(self, Params, best, a+1, variables)
+				best = rangeRecursive(self, Params, best, a+1, variables)
 			end
 		end
 
 		return best
 end
 
+local elementsRecursive 
+elementsRecursive = function(self, Params, best, a, variables)
+	forEachOrderedElement(Params[a]["elements"], function (idx, attribute, atype)
+		variables[Params[a]["id"]] = attribute
+		local mVariables = {} -- copy of the variables table to be used in the model.
+		forEachOrderedElement(variables, function(idx2, attribute2, atype2)
+			mVariables[idx2] = attribute2
+		end)
+
+		if a == #Params then
+			local m = self.model(mVariables) --testing the model with it's current parameter values.
+				m:execute(self.finalTime)
+				local candidate = self.fit(m)
+				if candidate < best then
+					best = candidate
+				end
+
+			else  -- else, go to the next parameter to test it with it's range of values.
+				best = elementsRecursive(self, Params, best, a+1, variables)
+		end
+	end)
+	return best
+end
 --@header Model Calibration functions.
 
 Calibration_ = {
@@ -44,20 +67,50 @@ Calibration_ = {
 	-- and then returns the parameter which generated the smaller fitness value.
 	-- @usage c:execute()
 	execute = function(self)
-		local startParams = {}
-		forEachOrderedElement(self.parameters, function(idx, attribute, atype)
-    		startParams[idx] = self.parameters[idx]["min"]
-		end)
-		local Params = {}
+		local rangedParameters = true
 		forEachOrderedElement(self.parameters, function (idx, attribute, atype)
-			Params[#Params+1] = {id = idx, min = self.parameters[idx]["min"], max = self.parameters[idx]["max"] }
+			if self.parameters[idx]["max"] == nil or self.parameters[idx]["min"]  == nil then
+				rangedParameters = false
+			end
 		end)
-		local m = self.model(startParams)
-		m:execute(self.finalTime)
-		local best = self.fit(m)
-		local variables = {}
-		best = executeRecursiveAux(self, Params, best, 1, variables)
-		return best
+
+		if rangedParameters == true then
+			local startParams = {}
+			forEachOrderedElement(self.parameters, function(idx, attribute, atype)
+    			startParams[idx] = self.parameters[idx]["min"]
+			end)
+
+			local Params = {}
+			forEachOrderedElement(self.parameters, function (idx, attribute, atype)
+				Params[#Params+1] = {id = idx, min = self.parameters[idx]["min"], max = self.parameters[idx]["max"] }
+			end)
+
+			local m = self.model(startParams)
+			m:execute(self.finalTime)
+			local best = self.fit(m)
+			local variables = {}
+			best = rangeRecursive(self, Params, best, 1, variables)
+			return best
+		else
+			local startParams = {}
+			forEachOrderedElement(self.parameters, function(idx, attribute, atype)
+    			startParams[idx] = self.parameters[idx][1]
+			end)
+
+			local Params = {}
+			forEachOrderedElement(self.parameters, function (idx, attribute, atype)
+				Params[#Params+1] = {id = idx, elements = attribute}
+			end)
+
+			local m = self.model(startParams)
+			m:execute(self.finalTime)
+			local best = self.fit(m)
+			local variables = {}
+			best = elementsRecursive(self, Params, best, 1, variables)
+			
+			return best
+			
+		end
 	end
 }
 
