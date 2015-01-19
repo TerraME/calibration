09745e105a797cf17092c3560880044ef7cc9b20
agojunior <junior092006@hotmail.com>
2015-01-19 16:03:57 -0200

    Made a File with the structure for testing each of the functions. However each test has not yet been implemented.

---
+++ tests/core/basic/SAMDE_.lua
@@ -0,0 +1,47 @@
+
+-- TO DO: Test each of the SAMDE functions
+return{
+
+	evaluate = function(unitTest)
+		unitTest:assert_equal(true)
+	end,
+	initPop = function(unitTest)
+		unitTest:assert_equal(true)
+	end,
+	g3Rand = function(unitTest)
+		unitTest:assert_equal(true)
+	end,
+	g4Rand = function(unitTest)
+		unitTest:assert_equal(true)
+	end,
+	copy = function(unitTest)
+		unitTest:assert_equal(true)
+	end,
+	copyParameters= function(unitTest)
+		unitTest:assert_equal(true)
+	end,
+	repareP = function(unitTest)
+		unitTest:assert_equal(true)
+	end,
+	oobTrea = function(unitTest)
+		unitTest:assert_equal(true)
+	end,
+	distancia = function(unitTest)
+		unitTest:assert_equal(true)
+	end,
+	normaliza = function(unitTest)
+		unitTest:assert_equal(true)
+	end,
+	maxVector = function(unitTest)
+		unitTest:assert_equal(true)
+	end,
+	maxDiversity = function(unitTest)
+		unitTest:assert_equal(true)
+	end,
+	SAMDE_ = function(unitTest)
+		unitTest:assert_equal(true)
+	end,
+	calibration= function(unitTest)
+		unitTest:assert_equal(true)
+	end
+	}
