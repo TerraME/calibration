b0fb83b47d1eaa8235a7414f765fd1c91932d90f
Antonio Gomes de Oliveira Junior <junior092006@hotmail.com>
2015-01-29 18:01:34 -0200

    New SAMDE example added

--- tests/core/basic/Calibration.lua
+++ tests/core/basic/Calibration.lua
@@ -17,15 +17,40 @@ local MyModel = Model{
 local c = Calibration{
 	model = MyModel,
 	finalTime = 1,
-	parameters = {x ={ -100, -1, 0, 1, 2, 100}, y = { min = 1, max = 10}},
+	parameters = {x ={-100, -1, 0, 1, 2, 100}, y = { min = 1, max = 10}},
+	fit = function(model)
+		return model.value
+	end
+}
+
+local MyModelSamde = Model{
+	x = choice{ min = 1, max = 10},
+	y = choice{ min = 1, max = 10},
+	init = function(self)
+		self.timer = Timer{
+			Event{action = function()
+				self.value = 2 * self.x ^2 - 3 * self.x + 4 + self.y
+			end}
+		}
+	end
+}
+
+local c2 = Calibration{
+	model = MyModelSamde,
+	finalTime = 1,
+	parameters = {x ={ min = 1, max = 10}, y = { min = 1, max = 10}},
+	SAMDE = true,
 	fit = function(model)
 		return model.value
 	end
 }
 
 local result = c:execute()
+local result2 = c2:execute()
+
 
 unitTest:assert_equal(result, 4)
+unitTest:assert_equal(result2, 4)
 
 end, 
 fit = function(unitTest)
@@ -36,4 +61,3 @@ execute = function(unitTest)
 		unitTest:assert(true)
 end
 }
-
