d1c12697d25376007709b2026f99bb2fe079eee2
Antonio Gomes de Oliveira Junior <junior092006@hotmail.com>
2015-01-29 18:00:55 -0200

    New Samde example added and bug fixes
    c (calibration) variables now are contained in the MyModel choice variables

--- examples/example.lua
+++ examples/example.lua
@@ -18,25 +18,37 @@ local MyModel = Model{
 c = Calibration{
 	model = MyModel,
 	finalTime = 1,
-	parameters = {x ={ min = -100, max = 100, step = 2}, y = { -1, 2 ,3}},
+	parameters = {x ={-100, -1, 0, 1, 2, 100}, y = { min = 1, max = 10}},
 	fit = function(model)
 		return model.value
 	end
 }
 
+local MyModelSamde = Model{
+	x = choice{ min = 1, max = 10},
+	y = choice{ min = 1, max = 10},
+	init = function(self)
+		self.timer = Timer{
+			Event{action = function()
+				self.value = 2 * self.x ^2 - 3 * self.x + 4 + self.y
+			end}
+		}
+	end
+}
 
--- local c3 = Calibration{
---	model = MyModel,
---  SAMDE = true,
---	finalTime = 1,
---	parameters = {x ={ min = -100, max = 100}, y = { min = 1, max = 10}},
---	fit = function(model)
---		return model.value
---	end
--- }
+local c2 = Calibration{
+	model = MyModelSamde,
+	finalTime = 1,
+	parameters = {x ={ min = 1, max = 10}, y = { min = 1, max = 10}},
+	SAMDE = true,
+	fit = function(model)
+		return model.value
+	end
+}
 
-result = c:execute()
-)
--- result3 = c3:execute()
+local result = c:execute()
+local result2 = c2:execute()
+
 
 print("result1: "..result)
+print("result2 (SAMDE): "..result)
