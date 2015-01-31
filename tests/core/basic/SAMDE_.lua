e76d16ccc1f7462e42b4206cad89d137ef6db6bb
agojunior <junior092006@hotmail.com>
2015-01-30 23:49:04 -0200

    Second Basic verification of SAMDE

--- tests/core/basic/SAMDE_.lua
+++ tests/core/basic/SAMDE_.lua
@@ -1,8 +1,7 @@
-
 -- TO DO: Test each of the SAMDE functions
 local MyModel
 MyModel = Model{
-	x = choice{-100, -1, 0, 1, 2, 100},
+	x = choice{ min = 1, max = 10},
 	y = choice{ min = 1, max = 10},
 	init = function(self)
 		self.timer = Timer{
@@ -13,17 +12,40 @@ MyModel = Model{
 	end
 }
 
+local varMatrix = {{1,10},{1,10}}
+local dim = 2
+local paramList = {"x","y"}
+local finalTime = 1
+local fit
+fit = function(model)
+	return model.value
+end
+
 return{
 
 	evaluate = function(unitTest)
-		local fit = function(model)
-			return model.value
-		end	
-		unitTest:assert_equal(evaluate({1,1}, 2, MyModel, {"x","y"}, 1, fit), 4)
+		unitTest:assert_equal(evaluate({1,1}, dim, MyModel, paramList, finalTime, fit), 4)
 	end,
+
 	initPop = function(unitTest)
-		unitTest:assert(true)
+		local pop = {}
+		local maxPopulation = 1
+		pop = initPop(maxPopulation, varMatrix, dim)
+		local result = true
+		for i=1, dim do
+			if not(pop[1][i] > varMatrix[i][1] and pop[1][i] < varMatrix[i][2]) then
+				result = false
+			end
+		end
+		
+		for i = dim + 1, dim + 12 do
+			if not(pop[1][i] < 1 and pop[1][i] > 0) then
+				result = false
+			end
+		end
+		unitTest:assert(result)
 	end,
+	
 	g3Rand = function(unitTest)
 		unitTest:assert(true)
 	end,
