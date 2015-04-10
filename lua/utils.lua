09fad21fd6a35a81969cc33d2b1ad935b907df1f
agojunior <junior092006@hotmail.com>
2015-04-10 09:34:46 -0300

    checkParameters function commented

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,88 +1,74 @@
--- local function valid(data, array)
---  local valid = {}
---  for i = 1, #array do
---   valid[array[i]] = true
---  end
---  if valid[data] then
---   return true
---  else
---   return false
---  end
--- end
-function checkParameters(tModel, tParameters)
-	mandatoryTableArgument(tParameters, "model", "Model")
-	mandatoryTableArgument(tParameters, "parameters", "table")
-	if type(tParameters) == "MultipleRuns" then
-		switch(tParameters.parameters, "strategy"):caseof{
-			factorial = function()
-			end,
-			repeated = function()
-				mandatoryTableArgument(tParameters, "quantity", "number")
-				forEachOrderedElement(tParameters.parameters,function(idx, att, typ)
-					if type(att) == "Choice" then
-						customError("Parameters used in repeated strategy must be a single variable and not a Choice Table")
-					end
-				end)
-			end,
-			selected = function()
-				forEachOrderedElement(tParameters.parameters,function(idx, att, typ)
-					mandatoryTableArgument(tParameters.parameters, att, "table")
-					forEachOrderedElement(tParameters.parameters,function(idx2, att2, typ2)
-						if type(att2) == "Choice" then
-							customError("Parameters used in each selected strategy scenario must be a single variable and not a Choice Table")
-						end
-					end)
-				end)
-			end,
-			sample = function()
-				mandatoryTableArgument(tParameters, "quantity", "number")
-				if data.parameters.seed ~= nil or data.model.seed ~= nil then
-    				customError("Models using repeated strategy cannot use random seed.")
-    			end	
-			end
-		}
-	end
-	local inChoices = true
-	forEachOrderedElement(tParameters.parameters, function (idx, att, typ)
-		if idx ~= "finalTime" and idx ~= "seed" then
-			print(idx)
-			print(type(tParameters.parameters))
-			print(type(att))
-			print(type(tParameters.parameters.idx))
-			if type(att) == "Choice" then
-				forEachOrderedElement(att.values, function(idx2, att2, typ2)
-					-- compares the values of the parameters in tParameters with the values in the model 
-					-- to check if all parameters are subset of models parameters.
-					inChoices = false
-					print(type(tModel))
-					print(type(tModel.idx))
-					forEachOrderedElement(tModel.idx.values, function(idx3, att3, typ3)
-						if tModel.idx.idx3 == att2 then
-							inChoices = true
-						end
-					end)
-					if inChoices == false then
-						customError("the value "..att2.." is not a valid choice for parameter "..idx)
-					end
-				end)
-			else
-				if  tParameters.parameters.idx.min > tModel.idx.min or tModel.idx.max > tParameters.parameters.idx.max then
-					customError("the value for"..idx.." is out of the range defined by the Model")
-				elseif type(tParameters.parameters.idx.step) == nil then
-					if type(tParameters) == "MultipleRuns" then
-						mandatoryTableArgument(tParameters.parameters[idx], idx..".step", "Choice")
-					elseif type(tModel.idx.step) ~= nil then
-						 customError(idx.."need a step value")
-					end
-				end
-			end
-		end
-	end)
+-- -- local function valid(data, array)
+-- --  local valid = {}
+-- --  for i = 1, #array do
+-- --   valid[array[i]] = true
+-- --  end
+-- --  if valid[data] then
+-- --   return true
+-- --  else
+-- --   return false
+-- --  end
+-- -- end
+-- function checkParameters(tModel, tParameters)
+-- 	mandatoryTableArgument(tParameters, "model", "Model")
+-- 	mandatoryTableArgument(tParameters, "parameters", "table")
+-- 	if type(tParameters) == "MultipleRuns" then
+-- 		switch(tParameters.parameters, "strategy"):caseof{
+-- 			factorial = function()
+-- 			end,
+-- 			repeated = function()
+-- 				mandatoryTableArgument(tParameters, "quantity", "number")
+-- 				forEachOrderedElement(tParameters.parameters,function(idx, att, typ)
+-- 					if type(att) == "Choice" then
+-- 						customError("Parameters used in repeated strategy must be a single variable and not a Choice Table")
+-- 					end
+-- 				end)
+-- 			end,
+-- 			selected = function()
+-- 				forEachOrderedElement(tParameters.parameters,function(idx, att, typ)
+-- 					mandatoryTableArgument(tParameters.parameters, att, "table")
+-- 					forEachOrderedElement(tParameters.parameters,function(idx2, att2, typ2)
+-- 						if type(att2) == "Choice" then
+-- 							customError("Parameters used in each selected strategy scenario must be a single variable and not a Choice Table")
+-- 						end
+-- 					end)
+-- 				end)
+-- 			end,
+-- 			sample = function()
+-- 				mandatoryTableArgument(tParameters, "quantity", "number")
+-- 				if data.parameters.seed ~= nil or data.model.seed ~= nil then
+--     				customError("Models using repeated strategy cannot use random seed.")
+--     			end	
+-- 			end
+-- 		}
+-- 	end
+-- 	forEachOrderedElement(tParameters.parameters, function (idx, att, typ)
+-- 		if idx ~= "finalTime" and idx ~= "seed" then
+-- 			print(idx)
+-- 			print(type(tParameters.parameters))
+-- 			print(type(att))
+-- 			print(type(tParameters.parameters.idx))
+-- 			if tModel.idx.min ~= nil then
+-- 				if (tParameters.parameters.idx.min - tModel.idx.min) % tParameters.parameters.idx.step then
+-- 					if tParameters.parameters.idx.min >= tModel.idx.min then
+-- 						if tParameters.parameters.idx.step ~= nil then
+-- 							if (tParameters.parameters.idx.min - tModel.idx.min) % tParameters.parameters.idx.step then
+
+-- 						end
+
+
+-- 				end 
+			 
+-- 			min(calibration) = min(model) + k * step(model), with k integer >= 0
+-- 			max(calibration) = max(model) + k * step(model), with k integer <= 0
+-- 			step(calibration) = step(model) * k, with k integer >= 0
+-- 		end
+-- 	end)
 	
-	-- tParameters.strategy
-	-- check each parameter for infinite possibilites
-	-- check if belongs to the model 
-end
+-- 	-- tParameters.strategy
+-- 	-- check each parameter for infinite possibilites
+-- 	-- check if belongs to the model 
+-- end
 
 function randomModel(tModel, tParameters)
 	-- The possible values for each parameter is being put in a table indexed by numbers.
