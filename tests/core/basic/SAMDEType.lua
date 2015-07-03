b14624aff4ee15fcceb264ae152c4881b66d4f22
agojunior <junior092006@hotmail.com>
2015-07-03 16:27:59 -0300

    parameter maximize for SAMDE tested and working close #52

--- tests/core/basic/SAMDEType.lua
+++ tests/core/basic/SAMDEType.lua
@@ -15,9 +15,19 @@ local c2 = SAMDE{
 	fit = function(model)
 		return model.value
 end}
+local c3 = SAMDE{
+	model = MyModelSamde,
+	parameters = {x = {min = 1, max = 10}, y = { min = 1, max = 10}},
+	maximize = true,
+	fit = function(model)
+		return model.value
+end}
 return{
 SAMDE = function(unitTest)
 unitTest:assertEquals(c2.fit, 4)
 unitTest:assertEquals(c2.instance.x, 1)
 unitTest:assertEquals(c2.instance.y, 1)
+unitTest:assertEquals(c3.fit, 184)
+unitTest:assertEquals(c3.instance.x, 10)
+unitTest:assertEquals(c3.instance.y, 10)
 end}
