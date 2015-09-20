bd9a5d890fbd7e899722251d301239a9a0933965
agojunior <junior092006@hotmail.com>
2015-09-20 02:07:50 -0300

    new goodnessof fit example

---
+++ examples/goodnessOfFit.lua
@@ -0,0 +1,15 @@
+import("calibration")
+
+		local cs12 = CellularSpace{
+			database = file("Costanza.map", "calibration"),
+			attrname = "Costanza"
+		}
+				local cs22 = CellularSpace{
+			database = file("Costanza2.map", "calibration"),
+			attrname = "Costanza"
+		}
+
+		local result = multiLevel(cs12, cs22, "Costanza")
+		local result2 = multiLevel(cs12, cs22, "Costanza", true)
+		print("result"..result3)
+		print("result2"..result4)
