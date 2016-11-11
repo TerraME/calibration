b233ef47eadf523ed7b6e355b2b0b31d49a6fb34
agojunior <junior092006@hotmail.com>
2016-11-11 11:43:32 -0200

    utils -> Utils

--- tests/core/alternative/utils.lua
+++ tests/core/alternative/utils.lua
@@ -26,5 +26,38 @@ return{
 		end
 		
 		unitTest:assertError(error_func, "Incompatible types. Argument '#1' expected table, got string.")
+	end,
+	checkParametersSet = function(unitTest)
+		error_func = function()
+			local parameters = {x = Choice{-100, 1, 3}}
+   			checkParametersSet(MyModel, "x", parameters.x)
+   		end
+   		unitTest:assertError(error_func, "Parameter 3 in #3 is out of the model x range.")
+	end,
+	checkParametersRange = function(unitTest)
+		error_func = function()
+			local parameters = {y = Choice{min = 2, max = 11, step =1}}
+	    	checkParametersRange(MyModel, "y", parameters.y)
+	    end
+	    unitTest:assertError(error_func, "Parameter y max is out of the model range.")
+
+	end,
+	checkParameterSingle = function(unitTest)
+		-- parameters = {x = Choice{-100, 2}}
+		error_func = function()
+	    	checkParameterSingle(MyModel, "x", 2, 5)
+	    end
+		 unitTest:assertError(error_func, "Parameter 5 in #2 is out of the model x range.")
+	end,
+
+	cloneValues = function(unitTest)
+		error_func = function()
+			local original = {x = 42}
+			local copy = clone(original)
+			if copy ~= original then
+				customError("Values are different.")
+			end
+		end
+		unitTest:assertError(error_func, "Values are different.")
 	end
 }
