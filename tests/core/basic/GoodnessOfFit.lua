d7da9d461557552c1fe4ade775233b486714a6b3
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2017-08-24 15:55:02 -0300

    Test #1963.

--- tests/core/basic/GoodnessOfFit.lua
+++ tests/core/basic/GoodnessOfFit.lua
@@ -49,6 +49,5 @@ return{
 		unitTest:assertEquals(result6, 0.62, 0.01)
 		unitTest:assertEquals(result9, 1, 0.01)
 		unitTest:assertEquals(result10, 1, 0.01)
-		unitTest:assertEquals(result10, 1, 0.01)
 	end
 }
