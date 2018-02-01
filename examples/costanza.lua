a8041389764fa945b0b4fd857d8a399b32da024c
Pedro Andrade <pedro.andrade@inpe.br>
2018-02-01 16:45:28 -0200

    Example to create figure more or less equal to Costanza's paper.

---
+++ examples/costanza.lua
@@ -0,0 +1,30 @@
+-- @example Basic example for testing goodness-of-fit.
+-- This example reproduces Figure 4 of from Costanza R. 
+-- Model goodness of fit: a multiple resolution procedure. Ecological modelling. 1989 Sep 15;47(3-4):199-215.
+import("calibration")
+
+scene1 = CellularSpace{
+	file = filePath("costanza.pgm", "calibration"),
+	attrname = "value"
+}
+
+scene2 = CellularSpace{
+	file = filePath("costanza2.pgm", "calibration"),
+	attrname = "value"
+}
+
+result = multiLevel{
+	target = {scene1, scene2},
+	select = "value",
+	discrete = true
+}
+
+print("Ft = "..result)
+
+chart = Chart{
+	target = data,
+	select = "fit",
+	xLabel = "Window Size",
+	yLabel = "Fit"
+}
+
